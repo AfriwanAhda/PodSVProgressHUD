372e5e7148e3e46f6f27f65eb3c792e75b1c0e64
ari <afriwan.phys@gmail.com>
2018-02-26 22:14:42 +0700

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+# platform :ios, '10.0'
+
+target 'pod-test' do
+  use_frameworks!
+
+  pod 'SVProgressHUD'
+
+end
---
+++ pod-test/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  pod-test
+//
+//  Created by Afriwan Ahda on 12/20/17.
+//  Copyright © 2017 Motion Studio. All rights reserved.
+//
+
+import UIKit
+import SVProgressHUD
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        SVProgressHUD.setDefaultMaskType(.black)
+        SVProgressHUD.show(withStatus: "Load Data...")
+        
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+          SVProgressHUD.dismiss()
+    }
+
+
+}
+
